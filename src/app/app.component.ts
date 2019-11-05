36589e11e324a2f33c5be84d3f6f49cc3f4897b6
alaksandarjesus <alaksandarjesus@yahoo.co.in>
2019-11-05 21:15:31 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,70 +14,70 @@ export class AppComponent implements OnInit {
     {
       question: "Question 1",
       answer: "Hello",
-      length: 0,
+      span: 0,
       x: -1,
       y: -1
     },
     {
       question: "Question 2",
       answer: "World",
-      length: 0,
+      span: 0,
       x: -1,
       y: -1
     },
     {
       question: "Question 3",
       answer: "Alaksanda",
-      length: 0,
+      span: 0,
       x: -1,
       y: -1
     },
     {
       question: "Question 4",
       answer: "Jesus",
-      length: 0,
+      span: 0,
       x: -1,
       y: -1
     },
     {
       question: "Question 5",
       answer: "Gene",
-      length: 0,
+      span: 0,
       x: -1,
       y: -1
     },
     {
       question: "Question 6",
       answer: "Maria",
-      length: 0,
+      span: 0,
       x: -1,
       y: -1
     },
     {
       question: "Question 7",
       answer: "Susai",
-      length: 0,
+      span: 0,
       x: -1,
       y: -1
     },
     {
       question: "Question 8",
       answer: "Bibiana",
-      length: 0,
+      span: 0,
       x: -1,
       y: -1
     },
     {
       question: "Question 9",
       answer: "Diana",
-      length: 0,
+      span: 0,
       x: -1,
       y: -1
     },
     {
       question: "Question 10",
       answer: "Kirubai",
-      length: 0,
+      span: 0,
       x: -1,
       y: -1
     }
@@ -85,51 +85,51 @@ export class AppComponent implements OnInit {
   // answer should not exceed 10 characters
   ngOnInit() {
     this.initTrs();
-    _.forEach(this.data, item => (item.length = item.answer.length));
-    this.data = _.reverse(_.sortBy(this.data, "length"));
-    console.log(this.data);
+    _.forEach(this.data, item => (item.span = item.answer.length));
+    this.data = _.reverse(_.sortBy(this.data, "span"));
     this.setFirstTds();
   }
 
   initTrs() {
     const tds = ["", "", "", "", "", "", "", "", "", ""];
 
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < 10; i++) {
       this.trs.push(tds);
     }
   }
 
   setFirstTds() {
     const first = this.data[0];
-   
-    const direction = this.directions[_.random(0, this.directions.length - 1)];
+
+    const direction:any = 'r'; //this.directions[_.random(0, this.directions.length - 1)];
+    console.log("direction", direction);
     let fits = false;
-    let count = 0;
-  //  do {
-    //   console.log("count", count);
-    //   const coordinates = this.getCoordinates();
-    //   switch (direction) {
-    //     case "t":
-    //       break;
-    //     case "r":
-    //       break;
-    //     case "b":
-    //       break;
-    //     case "l":
-    //       break;
-    // //   }
-    //   count++;
-    //   fits = fits || (count < 10);
-    // } while (fits);
-    console.log(this.trs)
+    for (let i = 0; i < 10; i++) {
+      const coordinates = this.getCoordinates();
+      
+      switch (direction) {
+        case "t":
+          break;
+        case "r":
+          if(first.span >= coordinates[1]){
+              console.log("span",first.span);
+              console.log(coordinates[0], coordinates[1]);
+          }
+          break;
+        case "b":
+          break;
+        case "l":
+          break;
+      }
+      
+    }
   }
 
-  getCoordinates(){
-     let x = _.random(0, 10);
+  getCoordinates() {
+    let x = _.random(0, 10);
     let y = _.random(0, 10);
     if (x < 0) x = x * -1;
     if (y < 0) y = y * -1;
-    return [x, y]
+    return [x, y];
   }
-  
 }
